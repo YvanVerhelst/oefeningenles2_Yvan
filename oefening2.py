d7c9d06315fa880975ab92a4fa0b139ddf9fde75
YvanVerhelst <yvan@motraco.be>
2023-12-07 20:18:31 +0100

    aanpassing python programma

--- oefening2.py
+++ oefening2.py
@@ -1,6 +1,6 @@
 getal1 = int(input('geef het eerste getal :'))
 getal2 = int(input('geef het tweede getal :'))
-som = getal1 + getal2
+som = getal1 + getal2 + 4
 produkt = getal1 * getal2
 print ('de som van',getal1,'en',getal2,'is', som)
 print ('het produkt van',getal1,'en',getal2,'is', produkt)
